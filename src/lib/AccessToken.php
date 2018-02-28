be49d452ce4f85031a1343931fc9662a588bf99c
Hollis_Ho <he_wenzhi@126.com>
2018-02-28 16:11:58 +0800

    Update AccessToken.php

--- src/lib/AccessToken.php
+++ src/lib/AccessToken.php
@@ -18,6 +18,29 @@ class AccessToken
      */
     public static function getMpToken($appId, $appSecret)
     {
+        if (!file_exists('./access_token.json')) fopen('./access_token.json', 'a+');
+        $file = file_get_contents('./access_token.json', true);
+        $result = json_decode($file, true);
+        if (!$result || time() > $result['expires']) {
+            $data = static::buildAccessToken($appId, $appSecret);
+            $data['expires'] = time() + $data['expires_in'] - 200;
+            $jsonStr =  json_encode($data);
+            $fp = fopen('./access_token.json', 'w');
+            fwrite($fp, $jsonStr);
+            return $data['access_token'];
+        } else {
+            return $result['access_token'];
+        }
+    }
+
+    /**
+     * 重新请求access_token
+     * @author Hollis Ho <he_wenzhi@126.com>
+     * @param $appId
+     * @param $appSecret
+     * @return mixed
+     */
+    protected static function buildAccessToken($appId, $appSecret) {
         $tokenAccessUrl = sprintf("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s", $appId, $appSecret);
         $arrContextOptions=array(
             "ssl"=>array(
@@ -26,7 +49,6 @@ class AccessToken
             ),
         );
         $res = file_get_contents($tokenAccessUrl, false, stream_context_create($arrContextOptions));
-        $result = json_decode($res, true);
-        return $result['access_token'];
+        return json_decode($res, true);
     }
 }
